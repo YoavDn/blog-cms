f544e05eafcf5649ac520e857d9d584eb4ecb8d0
yoav <yoavdan.m@gmail.com>
2022-12-24 01:38:04 +0200

    making tiptap stylings

---
+++ components/editor/TipTap.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  extensions: [StarterKit],
+  editorProps: {
+    attributes: {
+      class:
+        'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
+    },
+  },
+})
+</script>
+
+<template>
+  <editor-content class="p-2" :editor="editor" />
+</template>
--- pages/new.vue
+++ pages/new.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TipTap from '~~/components/editor/TipTap.vue'
+</script>
 
 <template>
-  <div class=""></div>
+  <div class="p-4 rounded-md">
+    <TipTap />
+  </div>
 </template>
 
 <style lang="scss"></style>
